59ff3fbc0d0b2ecb3841a70f0e6a99ae125408d2
Ninad-Patil <ninad.patil198@gmail.com>
2023-08-01 23:32:37 +0530

    deleted the onUpdate cascade constraint but still the deletion problem is not resolved

--- src/models/airport.js
+++ src/models/airport.js
@@ -12,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
       this.belongsTo(models.City, {
         foreignKey: "cityId",
         onDelete: "cascade",
-        onUpdate: "cascade",
       });
     }
   }
